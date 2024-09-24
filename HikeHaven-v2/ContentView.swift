367c631a15003dd411ffb960bcc6ddd56bbf069d
Diana Duong <dduong013@gmail.com>
2024-09-24 14:21:33 -0700

    Initial Commit

---
+++ HikeHaven-v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HikeHaven-v2
+//
+//  Created by Diana Duong on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HikeHaven-v2/HikeHaven_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  HikeHaven_v2App.swift
+//  HikeHaven-v2
+//
+//  Created by Diana Duong on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeHaven_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
